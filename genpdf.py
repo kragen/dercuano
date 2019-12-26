9e194aa2e63c6364ccd81dcf56fcef0673a47fdb
default <default@localhost.localdomain>
2019-12-26 04:25:27 -0300

    Make PDF generation change font sizes
    This is still super janky but it now looks at first glance like
    something a human could actually read, while previously it looked like
    someone accidentally rendered an ASCII text file with a proportional
    font and word wrap.

--- genpdf.py
+++ genpdf.py
@@ -5,21 +5,21 @@
 This is still just the crudest sketch of PDF generation for Dercuano.
 Missing pieces include:
 
-- larger fonts for headers
+- <pre>
+- Unicode subscripts (superscripts are OK)
 - bullets
+- Greek (all of "ε₀ ≈" is no good)
 - tables
-- Greek
 - Chinese
 - internal links (between pages)
 - external links (to URLs)
 - an outline that doesn't zoom you out to a whole page
 - font sizes
 - bold and italic
-- dingbats like ×
+- dingbats like × (no, that one is okay, also centered dot and degrees)
 - topic pages
 - main table of contents
 - JS tables of contents for individual notes
-- <pre>
 - a layout engine capable of handling varying font sizes in a line
 - chronological ordering
 - font fallbacks for missing characters
@@ -28,8 +28,10 @@ Missing pieces include:
 - maybe making the output file less than 6.8 megabytes?? not using
   base85 would fucking help
 - wrapping overlong lines so they don't get cut off
+- colored titles
+- hyphenation and justification
 
-It also takes three minutes to run on my netbook and generates a 3189-page PDF,
+It also takes three minutes to run on my netbook and generates a 3803-page PDF,
 so maybe some kind of output caching system would be useful.
 
 """
@@ -61,42 +63,71 @@ def start_page(c):
 
 def newline(c, t):
     t[0].textLine()
-    if t[0].getY() < bottom_margin:
+    if t[0].getY() < bottom_margin + 8*em:  # XXX fudge factor for layout bugs
         c.drawText(t[0])
         c.showPage()
         t[0] = start_page(c)
 
-def render_text(c, t, text):
-    max_y = pagesize[0]-right_margin
+def render_text(c, t, text, font):
+    max_y = pagesize[0] - right_margin
     words = text.split()
     for word in words:
-        width = c.stringWidth(word)
+        width = c.stringWidth(word, *font)
         if t[0].getX() + width > max_y:
             newline(c, t)
 
         t[0].textOut(word + ' ')
+
+block_fonts = {
+    'p': (roman, 1*em),
+    'h1': (roman, 2*em),
+    'h2': (roman, 1.7*em),
+    'h3': (roman, 1.4*em),
+    'h4': (roman, 1.2*em),
+    'h5': (roman, 1.1*em),
+    'h6': (roman, 1*em),
+    'li': (roman, 1*em),
+    }
+
+def push_font(t, font_stack, font):
+    font_stack.append(font)
+    t[0].setFont(*font)
 
 def render(filename, c, xml):
     c.bookmarkPage(filename)
     title = filename
     t = [start_page(c)]
     stack = [('element', xml.getroot())]
+    font_stack = [(roman, 1*em)]
     while stack:
         kind, obj = stack.pop()
+        new_font = False
         if kind == 'element':
-            if obj.tag in ('p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li'):
+            if obj.tag in block_fonts:
+                font = block_fonts[obj.tag]
+                size_diff = font[1] - font_stack[-1][1]
+                push_font(t, font_stack, font)
+                new_font = True
                 newline(c, t)
+                if size_diff > 0:
+                    t[0].moveCursor(0, size_diff * 1.2)
             if obj.tag == 'title':
                 title = obj.text
             if obj.text is not None:
-                render_text(c, t, obj.text)
+                render_text(c, t, obj.text, font_stack[-1])
             if obj.tail is not None:
                 stack.append(('text', obj.tail))
+            if new_font:  # must go atop the tail!
+                stack.append(('restorefont', None))
             for kid in reversed(list(obj)):
                 stack.append(('element', kid))
+        elif kind == 'text':
+            render_text(c, t, obj, font_stack[-1])
         else:
-            assert kind == 'text'
-            render_text(c, t, obj)
+            assert kind == 'restorefont'
+            font_stack.pop()
+            t[0].setFont(*font_stack[-1])
+            #newline(c, t)
 
     c.drawText(t[0])
     c.showPage()
